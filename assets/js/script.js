f6b493fa095a040a2a3fb2ebf675551c6d72deda
Niraj Limbu <nlimbu@yahoo.com>
2021-12-09 21:26:50 -0500

    add moment and jquery folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+// OpenWeather API key
+var apiKey = '6096ef2cb3daada49a2eef39c101f0e4';
 
+// Declare a variable to store the searched city
+var city = '';
+
+// variable declaration
+var searchCityEl = $('#search-city');
+var uvInputEl = $('#uv-input');
+var sCity = [];
